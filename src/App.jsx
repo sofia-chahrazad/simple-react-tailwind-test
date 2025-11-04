09824a465fccca99cd1fc91f2e1e33efaad20231
sofia-chahrazad <raslain.sofia@gmail.com>
2025-11-04 20:57:43 +0100

    visual changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,8 @@ import './App.css'
 // src/App.jsx
 export default function App() {
   return (
-    <div className="bg-white w-full min-h-screen pt-24"> {/* pt-24 = navbar height */}
       <Navbar />
-      <main>
-        <h1 className="text-2xl text-gray-800">Hello World</h1>
-      </main>
-    </div>
+      
+    
   );
 }
